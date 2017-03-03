7145d07990149b7a814edcdf2cd9821431f7c0b0
Liew JoonKui <ahkui@Liews-Mac.local>
2017-03-03 13:58:59 -0800

    add pod

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '10.0'
+use_frameworks!
+target 'currency' do
+pod 'Kanna', '~> 2.1.0'
+end
--- currency/ViewController.swift
+++ currency/ViewController.swift
@@ -7,12 +7,37 @@
 //
 
 import UIKit
+import Alamofire
+import Kanna
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        Alamofire.request("https://www.entiebank.com.tw/rate/page_host.asp").responseString(completionHandler: { response in
+            print("\(response.result.isSuccess)")
+            if let html = response.result.value {
+                                print(html)
+                
+                
+                
+                if let doc = HTML(html: html, encoding: .utf8) {
+                    print(doc.title)
+                    
+                    // Search for nodes by CSS
+                    for link in doc.css("a, link") {
+                        print(link.text)
+                        print(link["href"])
+                    }
+                    
+                    // Search for nodes by XPath
+                    for link in doc.xpath("//a | //link") {
+                        print(link.text)
+                        print(link["href"])
+                    }
+                }
+            }
+        })
     }
 
     override func didReceiveMemoryWarning() {
